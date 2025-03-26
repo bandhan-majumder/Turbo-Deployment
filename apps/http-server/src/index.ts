274706138e0bf0f1cb2dd51d336c260748ef1b54
bandhan-majumder <bandhanmajumder16@gmail.com>
2025-03-26 11:17:29 +0530

    add: repo initialized with initial code

---
+++ apps/http-server/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import { client } from "@repo/db/client";
+
+const app = express();
+app.use(express.json());
+
+app.get("/health", (req, res) => {
+    res.send("Hi there")
+})
+
+app.post("/signup", async (req, res) => {
+    const { username, password } = req.body;
+
+    // do database operations
+    try {
+        const user = await client.user.create({
+            data: {
+                username,
+                password
+            }
+        })
+
+        res.json({
+            success: "true",
+            message: "User created successfully",
+            userId: user.id
+        })
+    } catch (e) {
+        res.json({
+            success: "false",
+            message: "Could not create user",
+        })
+    }
+})
+app.listen(3000);
